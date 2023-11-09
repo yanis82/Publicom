2f7291ea49db8251ff571d917ecb4130586f7566
Lucas SANCHEZ <l.sanchez@BTSSIO82.local>
2023-11-09 12:05:18 +0100

    email

--- app/Controllers/Utilisateur.php
+++ app/Controllers/Utilisateur.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\UtilisateurModel;
 
 
@@ -22,82 +23,102 @@ class Utilisateur extends BaseController
     public function creer()
     {
         $mdp = $this->request->getPost('pass');
-        $hashMdp = password_hash($mdp, PASSWORD_BCRYPT);
-        $inputs = [
-            'nomUtilisateur' => $this->request->getPost('nom'),
-            'prenomUtilisateur' => $this->request->getPost('prenom'),
-            'emailUtilisateur' => strtolower($this->request->getPost('email')),
-            'mdpUtilisateur' => $hashMdp,
-            'verifPass' => $this->request->getPost('verifPass'),
-        ];
-        $validateUser = $this->validateUser($inputs);
-        array_push($inputs, ['isADmin' => 0]);
-        if($validateUser[0]){
+        $verifMdp = $this->request->getPost('verifPass');
+        if ($mdp === $verifMdp) {
             $UtilisateurModel = new UtilisateurModel();
-            $isAlreadyExistant = $UtilisateurModel -> where(['EMAILUTILISATEUR' => $inputs['emailUtilisateur']]) -> first();
-            if($isAlreadyExistant) {
-                return Utilitaires::error('Utilisateur déjà existant');
+            
+            $hashMdp = password_hash($mdp, PASSWORD_BCRYPT);
+            $inputs = [
+                'nomUtilisateur' => $this->request->getPost('nom'),
+                'prenomUtilisateur' => $this->request->getPost('prenom'),
+                'emailUtilisateur' => strtolower($this->request->getPost('email')),
+                'mdpUtilisateur' => $hashMdp,
+                'verifPass' => $this->request->getPost('verifPass'),
+            ];
+            
+            $isAlreadyEmail = $UtilisateurModel->where(['emailUtilisateur' => $inputs['emailUtilisateur']])->find();
+            if($isAlreadyEmail){
+
+            
+
+            $validateUser = $this->validateUser($inputs);
+            array_push($inputs, ['isADmin' => 0]);
+            if ($validateUser[0]) {
+                $isAlreadyExistant = $UtilisateurModel->where(['EMAILUTILISATEUR' => $inputs['emailUtilisateur']])->first();
+                if ($isAlreadyExistant) {
+                    return Utilitaires::error('Utilisateur déjà existant');
+                }
+                
+                $UtilisateurModel->insert($inputs);
+                return Utilitaires::success('Utilisateur cree avec succes');
+            } else {
+                return Utilitaires::error('Erreur, tous les champs doivent etre remplis');
             }
-            $UtilisateurModel->insert($inputs);
-
-            $res = ['message' => 'ok'];
-            return Utilitaires::success('Utilisateur cree avec succes');
-        }else {
-
-            return Utilitaires::error('Erreur lors de la creation de l\'utilisateur');
+        } else
+        } else {
+            return Utilitaires::error('Erreur, les mot de passes ne correspondent ');
         }
     }
 
-    public function seConnecterForm() {
+    public function seConnecterForm()
+    {
         return view('seConnecter');
     }
 
-    public function seConnecter() {
+    public function seConnecter()
+    {
         $utilisateurModel = new UtilisateurModel();
         $datas = [
-            'email' => $this -> request -> getPost('email'),
-            'pass' => $this -> request -> getPost('pass'),
+            'email' => $this->request->getPost('email'),
+            'pass' => $this->request->getPost('pass'),
         ];
 
-        $user = $utilisateurModel -> where(['emailUtilisateur' => $datas['email']]) -> first();
+        $user = $utilisateurModel->where(['emailUtilisateur' => $datas['email']])->first();
 
-        if(!$user) return Utilitaires::error('email ne correspond à aucun utilisateur');
+        if (!$user)
+            return Utilitaires::error('email ne correspond à aucun utilisateur');
 
         $passwordOk = password_verify($datas['pass'], $user['MDPUTILISATEUR']);
-        if($passwordOk){
-            session() -> set(['isConnected' => $user]);
+        if ($passwordOk) {
+            session()->set(['isConnected' => $user]);
             return Utilitaires::success('connecté avec succès', 'liste-messages');
         } else {
             return Utilitaires::error('connexion échoué: mot de passe ou email incorrect');
         }
     }
 
-    public function logout() {
+    public function logout()
+    {
         session()->remove('isConnected');
         return Utilitaires::success('Deconnecté avec succès', 'se-connecter');
     }
 
-    public function testDev() {
+    public function testDev()
+    {
         return json_encode([
-            'session() -> has()' => session() -> has('isConnected'),
-            'session() -> get()' => session() -> get(),
+            'session() -> has()' => session()->has('isConnected'),
+            'session() -> get()' => session()->get(),
         ]);
     }
 
-    static function isConnected(): bool {
-        return session() -> has('isConnected');
-    }
-    private function validateUser($inputs) 
+    static function isConnected(): bool
     {
-        
+        return session()->has('isConnected');
+    }
+
+    // si un champ est vide, renvoi un tableau dont ke preimer element est vide avec 'error' => erreur
+    //sinon renvoin [true]
+    private function validateUser($inputs)
+    {
+
         foreach ($inputs as $key => $value) {
-            if(empty($value)){
+            if (empty($value)) {
                 return [false, 'error' => "$key est vide"];
             }
         }
-        return [true];   
+        return [true];
     }
 
 
-    
+
 }
