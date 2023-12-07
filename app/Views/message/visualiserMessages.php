5adb4419015dc58ffab1eea096e98e74ce324346
lucas <you@example.com>
2023-12-07 12:03:38 +0100

    carou marche pas

--- app/Views/message/visualiserMessages.php
+++ app/Views/message/visualiserMessages.php
@@ -15,19 +15,31 @@ Visualiser Messages
 <?= $this->section('main') ?>
 <div class="container">
     <h1>Visualiser Messages</h1>
-    <div id="visualiserContainer"
-        >
-        <?php foreach ($data as $message): ?>
-            <div class="visualiserItem" style="background-image: url('<?= base_url('images/' . $message['IMAGEMESSAGE']) ?>')">
 
-                <h2>
-                    <?= $message['TITREMESSAGE']; ?>
-                </h2>
-                <p>
-                    <?= $message['CONTENUMESSAGE']; ?>
-                </p>
-            </div>
-        <?php endforeach; ?>
-    </div>
+
+
+    <section class="carousel" aria-label="Gallery">
+        <ol class="carousel__viewport">
+            <?php foreach ($data as $key => $message): ?>
+                <!-- <div class="carousel-item active">
+                    <img class="d-block w-100" src="<?= base_url('images/' . $message['IMAGEMESSAGE']) ?>" alt="slide">
+                </div> -->
+                <li id="carousel__slide<?= $key ?>" tabindex="0" class="carousel__slide">
+                    <div class="carousel__snapper">
+                        <p><?= $key ?></p>
+                        <p>count<?= count($data) ?></p>
+                        <p>suiv : <?= $key + 1 >= count($data) ? 0 : $key +1 ?></p>
+                        <a href="#carousel__slide<?= $key - 1 <= -1 ? count($data)-1 : $key-1 ?>" class="carousel__prev">Go
+                            to last slide</a>
+                        <a href="#carousel__slide<?= $key + 1 >= count($data) ? 0 : $key +1 ?>" class="carousel__next">Go to next slide</a>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+
+        </ol>
+
+        <!-- <div id="visualiserContainer">
+        
+    </div> -->
 </div>
 <?= $this->endSection() ?>
